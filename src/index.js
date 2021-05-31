87956e0fd10cfe8611c458d97aa3a70a0d69f554
nonuabi <nonuabi@gmail.com>
2021-05-31 13:34:05 +0530

    added middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 
 import "./index.css";
 import App from "./components/App";
 import combineReducers from "./reducers";
 
-const store = createStore(combineReducers);
+//function logger(obj, next, action)
+
+const logger = function ({ dispatch, getState }) {
+  return function (next) {
+    return function (action) {
+      //middleware code
+      console.log("Action_type = ", action.type);
+      next(action);
+    };
+  };
+};
+
+const store = createStore(combineReducers, applyMiddleware(logger));
 console.log("store :: ", store);
 console.log("Before State :: ", store.getState());
 
